48335e10b4c736a25d9aeb1a8d675b2db2e60d24
Chanokchon Wongjumpa <mint.rosetta2001@gmail.com>
2023-12-30 13:11:45 +0700

    FindOdd

---
+++ Codewar/src/Problems/FindOdd.java
@@ -0,0 +1,27 @@
+package Problems;
+
+import java.util.Arrays;
+
+// source: https://www.codewars.com/kata/54da5a58ea159efa38000836/train/java
+public class FindOdd {
+    public static int solve(int[] a) {
+        Arrays.sort(a);
+
+        int current = a[0];
+        int count = 1;
+        for (int index = 1; index < a.length; index++) {
+            if (current == a[index]) {
+                count++;
+            } else {
+                if (count % 2 != 0) {
+                    break;
+                } else {
+                    current = a[index];
+                    count = 1;
+                }
+            }
+        }
+
+        return current;
+    }
+}
