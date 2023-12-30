e7fa67deec45806dfb2ba7e59004a7fb949f2f83
Chanokchon Wongjumpa <mint.rosetta2001@gmail.com>
2023-12-30 13:13:47 +0700

    Update Main.java

--- Codewar/src/Main.java
+++ Codewar/src/Main.java
@@ -1,5 +1,7 @@
+import Problems.Order;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Order.solve("4of Fo1r pe6ople g3ood th5e the2"));
     }
 }
