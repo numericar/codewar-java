2ee9207245b4b14e0dc292a24c8da1fdec3ceb2c
Chanokchon Wongjumpa <mint.rosetta2001@gmail.com>
2023-12-31 23:19:59 +0700

    initial PilotCubes

--- Codewar/src/Main.java
+++ Codewar/src/Main.java
@@ -1,9 +1,7 @@
-import Problems.DigPow;
-import Problems.FindOutlier;
+import Problems.PilotCubes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(DigPow.solve(695, 2));
-        System.out.println(DigPow.solve(46288, 3));
+        System.out.println(PilotCubes.solve(0L));
     }
 }
