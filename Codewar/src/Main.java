84a22b808f4a70db984a3844a2dba6075f310cae
Chanokchon Wongjumpa <mint.rosetta2001@gmail.com>
2023-12-31 14:58:45 +0700

    FindOutlier

--- Codewar/src/Main.java
+++ Codewar/src/Main.java
@@ -1,7 +1,8 @@
 import Problems.Diamond;
+import Problems.FindOutlier;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Diamond.solve(5));
+        System.out.println(FindOutlier.solve(new int[] { 2, 4, 0, 100, 4, 11, 2602, 36 }));
     }
 }
