daa2d4ea0270188676ef17e6c98db4eba03799bb
Chanokchon Wongjumpa <mint.rosetta2001@gmail.com>
2024-01-01 02:32:53 +0700

    Update DigPow.java

--- Codewar/src/Problems/DigPow.java
+++ Codewar/src/Problems/DigPow.java
@@ -1,6 +1,10 @@
 package Problems;
 
 // source: https://www.codewars.com/kata/5552101f47fc5178b1000050/train/java
+
+/*
+    Armstrong number
+*/
 public class DigPow {
     public static long solve(int n, int p) {
         String numberText = Integer.toString(n);
@@ -15,9 +19,10 @@ public class DigPow {
         }
 
         double sumOfNP = Math.pow(n, p);
-        if (result == sumOfNP) {
+        if (result == sumOfNP) { // ถ้าหาก result มีค่าเท่ากับ sumOfNP, เราสามารถ return p ได้เลย
             return p;
         } else if (result % n == 0) {
+            /* หาตัวเลขที่ใช้ในการคูณเพื่อที่จะได้ผลลัพธ์ โดยเราสามารถนำ result / n เพื่อหาว่าเลขอะไรที result คูณแล้วมีค่าเท่ากับ sumOfNP กันแล้วได้ result */
             return result / n;
         } else {
             return -1;
