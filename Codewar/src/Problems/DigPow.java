d971bf6f5443fc4ded817c2e6a1780ccc20eb97a
Chanokchon Wongjumpa <mint.rosetta2001@gmail.com>
2023-12-31 22:53:46 +0700

    DigPow

--- Codewar/src/Problems/DigPow.java
+++ Codewar/src/Problems/DigPow.java
@@ -3,6 +3,24 @@ package Problems;
 // source: https://www.codewars.com/kata/5552101f47fc5178b1000050/train/java
 public class DigPow {
     public static long solve(int n, int p) {
-        return 0L;
+        String numberText = Integer.toString(n);
+
+        long result = 0L;
+        int pow = p;
+        for (int index = 0; index < numberText.length(); index++) {
+            int number = (numberText.charAt(index) - '0');
+
+            result += (long) Math.pow(number, pow);
+            pow++;
+        }
+
+        double sumOfNP = Math.pow(n, p);
+        if (result == sumOfNP) {
+            return p;
+        } else if (result % n == 0) {
+            return result / n;
+        } else {
+            return -1;
+        }
     }
 }
